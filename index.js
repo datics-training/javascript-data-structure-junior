7a94c9674b666ed92ef2b895295d357c025cde97
Datmaniac <>
2022-11-04 12:27:05 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// You have two arrays. One shows the names of the people names, while the other shows their occupation jobs. Your task is to create an object displaying each person to their respective occupation.
+
+/* for example
+const names = ["Dennis", "Vera", "Mabel", "Annette", "Sussan"]
+const jobs = ["Butcher", "Programmer", "Doctor", "Teacher", "Lecturer"]
+ 
+assignPersonToJob(names, jobs) should return {
+  Dennis: "Butcher",
+  Vera: "Programmer",
+  Mabel: "Doctor",
+  Annette: "Teacher",
+  Sussan: "Lecturer"
+}
+*/
+
+// Please write the code to fulfill the requirement and explain your implementation
